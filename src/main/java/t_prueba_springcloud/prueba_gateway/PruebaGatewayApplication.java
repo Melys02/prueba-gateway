c00734bb563c393a1930b7f6c777b2de09c3a48e
Melys02 <melys0919@gmail.com>
2024-10-11 22:59:56 -0500

    inicio

---
+++ src/main/java/t_prueba_springcloud/prueba_gateway/PruebaGatewayApplication.java
@@ -0,0 +1,13 @@
+package t_prueba_springcloud.prueba_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/t_prueba_springcloud/prueba_gateway/PruebaGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package t_prueba_springcloud.prueba_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
